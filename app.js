afa759b9714dd582928dd3598c2e3663e82fd581
Danh Phan <phandanh2112@gmail.com>
2023-10-01 22:14:56 +0700

    ver 1.5 - do advertisement

---
+++ app.js
@@ -0,0 +1,35 @@
+$(document).ready(function () 
+{
+  $(".image-slider").slick
+  ({
+    slidesToShow: 1, 
+    slidesToScroll: 1, 
+    infinite: true,
+    arrows: true,  
+    draggable: false, 
+    autoplay: true,
+    autoplaySpeed: 1500,
+    prevArrow: `<button type='button' class='slick-prev slick-arrow'><ion-icon name="arrow-back-outline"></ion-icon></button>`,
+    nextArrow: `<button type='button' class='slick-next slick-arrow'><ion-icon name="arrow-forward-outline"></ion-icon></button>`,
+    dots: true,
+    responsive: 
+    [
+      {
+        breakpoint: 1025,
+        settings: 
+        {
+          slidesToShow: 1,
+          arrows: false, 
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: 
+        {
+          slidesToShow: 1,
+          arrows: false,  
+        },
+      },
+    ],
+  });
+});
